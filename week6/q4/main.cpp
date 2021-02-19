6d56e7c32ab84db9eb422dbbacb4fe192efce4e0
Karen HY Tu <karenhaoyitu@gmail.com>
2021-02-19 23:50:46 +0800

    Implement the printDivisors function

---
+++ week6/q4/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void printDivisors(int num);
+
+int main() {
+    int inputNumber;
+    cout << "Please enter a positive integer >=2: ";
+    cin >> inputNumber;
+    printDivisors(inputNumber);
+    return 0;
+}
+
+void printDivisors(int num) {
+    int pairDivisor = num;
+    for (int divisor = 1; divisor * divisor <= num; divisor++) {
+        if (num % divisor == 0 && pairDivisor >= divisor) {
+            pairDivisor = num / divisor;
+            cout << divisor << " ";
+
+        }
+    }
+
+    while (pairDivisor * pairDivisor > 0) {
+        int divisor = num / pairDivisor;
+        if (num % pairDivisor == 0 && divisor > pairDivisor) {
+            cout << divisor << " ";
+        }
+        pairDivisor--;
+    }
+    cout << endl;
+}
+
