dba2299fa946050019e93b6a67f1457fad86b2cc
Karen HY Tu <karenhaoyitu@gmail.com>
2021-03-19 04:10:37 +0800

    Implement readIndexInDynamicArray funciton

--- week10/q3/main.cpp
+++ week10/q3/main.cpp
@@ -4,29 +4,40 @@
 
 using namespace std;
 
-//int main1();
+int main1();
 
 int main2();
 
 void readIndexInVector(vector<int> inputArray, int searchValue);
 
+void readIndexInDynamicArray(const int *inputArray, int inputArraySize, int searchValue);
+
 int main() {
-//    main1();
+    main1();
     main2();
     return 0;
 }
 
-//int main1() {
-//    cout << "Please enter a sequence of positive integers, each in a separate line." << endl;
-//    cout << "End you input by typing -1." << endl;
-//
-//    int searchValue;
-//    cout << "Please enter a number you want to search." << endl;
-//    cin >> searchValue;
-//    cout << searchValue << " shows in lines ";
-//    cout << endl;
-//    return 0;
-//}
+int main1() {
+    cout << "Please enter a sequence of positive integers, each in a separate line." << endl;
+    cout << "End you input by typing -1." << endl;
+    int size = 1;
+    int *inputArray = new int[size];
+    int currentInput = 0;
+    while (currentInput != -1) {
+        cin >> currentInput;
+        inputArray[size - 1] = currentInput;
+        size++;
+    }
+    int searchValue;
+    cout << "Please enter a number you want to search." << endl;
+    cin >> searchValue;
+    cout << searchValue << " shows in lines ";
+    readIndexInDynamicArray(inputArray, size, searchValue);
+    cout << endl;
+    delete[] inputArray;
+    return 0;
+}
 
 int main2() {
     vector<int> inputArray;
@@ -48,6 +59,17 @@ int main2() {
     return 0;
 }
 
+void readIndexInDynamicArray(const int *inputArray, int inputArraySize, int searchValue) {
+    int lineIndex = 0;
+    for (int i = 0; i < inputArraySize; i++) {
+        if (inputArray[i] == searchValue) {
+            lineIndex == 0 && cout << i + 1;
+            lineIndex > 0 && cout << ", " << i + 1;
+            lineIndex++;
+        }
+    }
+    cout << "." << endl;
+}
 
 void readIndexInVector(vector<int> inputArray, int searchValue) {
     int lineIndex = 0;
@@ -59,4 +81,5 @@ void readIndexInVector(vector<int> inputArray, int searchValue) {
         }
     }
     cout << "." << endl;
+
 }
